c92b850326385c5e4f8481192a2b66734b595f52
Barghouda Sihem <sihembarghouda93@gmail.com>
2025-05-11 15:43:06 +0100

    first commit

---
+++ discovery-service/src/main/java/com/example/discovery/DiscoveryServiceApplication.java
@@ -0,0 +1,14 @@
+// discovery-service/src/main/java/com/example/discovery/DiscoveryServiceApplication.java
+package com.example.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServiceApplication.class, args);
+    }
+}
